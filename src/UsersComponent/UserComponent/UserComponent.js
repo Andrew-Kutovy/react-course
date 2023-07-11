03541c0e20106f730f0ecddab80aabef1ecff0bc
Andrew-Kutovy <andrijkutovij5@gmail.com>
2023-07-11 14:33:17 +0300

    complete --------

--- src/UsersComponent/UserComponent/UserComponent.js
+++ src/UsersComponent/UserComponent/UserComponent.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './UserComponent.module.css';
 
-const UserComponent = (propt) => {
-    let {user} = propt
+const UserComponent = (props) => {
+    const { user } = props;
+    const [posts, setPosts] = useState([]);
+    const [showPosts, setShowPosts] = useState(false);
+
+    const fetchPosts = () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+            .then((res) => res.json())
+            .then((posts) => setPosts(posts));
+        setShowPosts(true);
+    };
+
+    const togglePosts = () => {
+        if (showPosts) {
+            setPosts([]);
+        }
+        setShowPosts(!showPosts);
+    };
+
     return (
         <div className={styles.container}>
-            <p>{user.name}</p>
-            <button onClick={()=>{
-                console.log('click')
-            }}>show posts</button>
+            <h2>{user.name}</h2>
+            {showPosts ? (
+                <>
+                    <button onClick={togglePosts}>Hide posts</button>
+                    {posts.length > 0 && (
+                        <ul>
+                            {posts.map((post) => (
+                                <li key={post.id}>{post.title}</li>
+                            ))}
+                        </ul>
+                    )}
+                </>
+            ) : (
+                <button onClick={fetchPosts}>Show posts</button>
+            )}
         </div>
     );
 };
--- src/UsersComponent/UsersComponent.js
+++ src/UsersComponent/UsersComponent.js
@@ -1,9 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import UserComponent from "./UserComponent/UserComponent";
-import styles from './UsersComponent.module.css'
 
 const UsersComponent = () => {
     const [users, setUsers] = useState([])
+
 
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -12,12 +12,7 @@ const UsersComponent = () => {
     }, [])
 
 
-    return (
-        <div className={styles.container}>
-            {users.map((user, id) => <UserComponent key={id} user={user} />)}
-        </div>
-
-    );
+    return (<>{users.map((user) => (<UserComponent key={user.id} user={user} />))}</>)
 };
 
 export default UsersComponent;
