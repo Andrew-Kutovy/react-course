013a17f8ba35a838141fe0148b6d002fbb1e2283
Andrew-Kutovy <andrijkutovij5@gmail.com>
2023-07-16 11:58:52 +0300

    empty -------- comit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { createContext, useState } from 'react';
-import Child1 from './Сomponents/Child1Component/Child1';
-import Child2 from './Сomponents/Child2Component/Child2';
+import React from 'react';
 
-export const DataContext = createContext();
 
 const App = () => {
-    const [data, setData] = useState(null);
 
     return (
         <div>
             <h1>App</h1>
-            <DataContext.Provider value={{ data, setData }}>
-                <Child1 />
-                <Child2 />
-            </DataContext.Provider>
         </div>
     );
 };
