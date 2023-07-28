d7f7dfa8c812f57a9a0c99c4673380e074bcf242
home <andrijkutovij5@gmail.com>
2023-07-28 17:57:09 +0300

    movie details genre stars - pagination

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,11 +1,9 @@
-
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import StarRatings from 'react-star-ratings';
-
 import DetailService from '../../services/detailService';
 
-const MovieInfo = () => { // Заменили "DetailsPage" на "MovieInfo"
+const MovieInfo = () => {
     const { movieId } = useParams();
     const [movieDetails, setMovieDetails] = useState(null);
 
@@ -23,19 +21,30 @@ const MovieInfo = () => { // Заменили "DetailsPage" на "MovieInfo"
         return <div>Loading...</div>;
     }
 
+    console.log(movieDetails);
     return (
         <div>
             <h1>{movieDetails.title}</h1>
-            {/* Добавляем компонент StarRatings и передаем в него свойство rating */}
             <StarRatings
-                rating={movieDetails.vote_average / 2} // Разделим на 2, так как максимальный рейтинг - 10, а максимальное количество звезд - 5
-                starRatedColor="#f1c40f" // Цвет активных звезд (здесь используется желтый цвет)
-                starEmptyColor="#dcdcdc" // Цвет неактивных звезд (здесь используется светло-серый цвет)
-                starDimension="30px" // Размер звезд (по умолчанию 40px)
-                starSpacing="2px" // Расстояние между звездами (по умолчанию 0)
+                rating={movieDetails.vote_average / 2}
+                starRatedColor="#f1c40f"
+                starEmptyColor="#dcdcdc"
+                starDimension="30px"
+                starSpacing="2px"
             />
+            <div>
+                <strong>Genres: </strong>
+                {movieDetails.genres.map((genre) => (
+                    <Link key={genre.id} to={`/genres/${genre.id}`}>
+                        {genre.name} /
+                    </Link>
+                ))}
+            </div>
             <p>{movieDetails.overview}</p>
-            <img src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`} alt="poster" />
+            <img
+                src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`}
+                alt="poster"
+            />
         </div>
     );
 };
