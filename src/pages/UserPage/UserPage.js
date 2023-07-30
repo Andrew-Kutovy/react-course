0a97dd21bda81b26c4d7685d6117f95c11f112d1
home <andrijkutovij5@gmail.com>
2023-07-31 00:34:14 +0300

    movie details genre stars pagination user

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -3,7 +3,10 @@ import React from 'react';
 const UserPage = () => {
     return (
         <div>
-            User's info:
+            <h2>User's info:</h2>
+            <h3>name: Andrew Kutovy</h3>
+            <h3><a href="https://github.com/Andrew-Kutovy/react-course/tree/movies">GitHub</a></h3>
+
         </div>
     );
 };
