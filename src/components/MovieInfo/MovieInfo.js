4a07b2081330a26666fa21e3ca6101713207399f
home <andrijkutovij5@gmail.com>
2023-07-31 17:53:02 +0300

    all complete

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -3,6 +3,7 @@ import { useParams, Link } from 'react-router-dom';
 import StarRatings from 'react-star-ratings';
 
 import DetailService from '../../services/detailService';
+import style from './MovieInfo.module.css';
 
 const MovieInfo = () => {
     const { movieId } = useParams();
@@ -23,25 +24,19 @@ const MovieInfo = () => {
     }
 
     return (
-        <div>
-            <h1>{movieDetails.title}</h1>
-            <StarRatings
-                rating={movieDetails.vote_average / 2}
-                starRatedColor="#f1c40f"
-                starEmptyColor="#dcdcdc"
-                starDimension="30px"
-                starSpacing="2px"
-            />
+        <div className={style.container} >
+            <img className={style.poster} src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`} alt="poster"/>
             <div>
-                <h2>Genres: </h2>
+                <h1>{movieDetails.title}</h1>
+                <StarRatings rating={movieDetails.vote_average / 2} starRatedColor="#f1c40f" starEmptyColor="#dcdcdc" starDimension="30px" starSpacing="2px"/>
+                <h3>Genres:</h3>
                 {movieDetails.genres.map((genre) => (
-                    <Link key={genre.id} to={`/genres/${genre.id}`}>{genre.name} /</Link>
+                    <Link className={style.genres} key={genre.id} to={`/genres/${genre.id}`}>{genre.name} </Link>
                 ))}
                 <h3>{movieDetails.tagline}</h3>
                 <h3>release date: {movieDetails.release_date}</h3>
+                <p>{movieDetails.overview}</p>
             </div>
-            <p>{movieDetails.overview}</p>
-            <img src={`https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`} alt="poster"/>
         </div>
     );
 };
